a1ff3a0ea9427a17ad841f9d737b2cece917988c
Pedro Bonfá <pedro.bonfa@estudante.ufjf.br>
2024-12-02 20:49:47 -0300

    adiciona mais animais

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,8 @@ function App() {
     <>
       <div className='app'>
         <Animal icone='🦁' nome='Leão' peso={190.37} extincao={true}/>
+        <Animal icone='🦒' nome='Girafa' peso={1200} extincao={true}/>
+        <Animal icone='🦓' nome='Zebra' peso={200}/>
       </div>
     </>
   )
