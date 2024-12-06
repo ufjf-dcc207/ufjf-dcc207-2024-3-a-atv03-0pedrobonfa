5bcb4376b7aac6127498aad08d511d1a07826cb8
Pedro Bonfá <pedro.bonfa@estudante.ufjf.br>
2024-12-05 21:49:09 -0300

    adiciona template de exibição

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,22 @@
 import './App.css'
 import Animal from './Animal'
+import Exibicao from "./Exibicao";
 function App() {
 
-  return (
-    <>
-      <div className='app'>
-        <Animal icone='🦁' nome='Leão' peso={190.37} extincao={true}/>
-        <Animal icone='🦒' nome='Girafa' peso={1200} extincao={true}/>
-        <Animal icone='🦓' nome='Zebra' peso={200}/>
-      </div>
-    </>
+  return ( 
+    <div className='app'>
+      <Exibicao abertura={new Date("2024-12-06T08:00:00.000-03:00")} fechamento={new Date("2024-12-06T16:00:00.000-03:00")}
+      cercado='AI'/>
+
+
+      <Animal icone='🦁' nome='Leão' peso={190.37} extincao={true}/>
+      <Animal icone='🦒' nome='Girafa' peso={1200} extincao={true}/>
+      <Exibicao abertura={new Date("2024-12-06T08:00:00.000-03:00")} fechamento={new Date("2024-12-06T16:00:00.000-03:00")}
+      cercado='
+      BI' />
+      <Animal icone='🦓' nome='Zebra' peso={200}/>
+    </div>
+    
   )
 }
 
