b37a26208daf6b9a07f096e996dd45970b7c6b99
Pedro Bonfá <pedro.bonfa@estudante.ufjf.br>
2024-12-05 22:00:14 -0300

    implementa animais dentro da exibicao

--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,21 @@ function App() {
 
   return ( 
     <div className='app'>
-      <Exibicao abertura={new Date("2024-12-06T08:00:00.000-03:00")} fechamento={new Date("2024-12-06T16:00:00.000-03:00")}
-      cercado='AI'/>
-
-
+      <Exibicao abertura={new Date("2024-12-06T08:00:00.000-03:00")} 
+      fechamento={new Date("2024-12-06T16:00:00.000-03:00")}
+      cercado='A1'
+      >
       <Animal icone='🦁' nome='Leão' peso={190.37} extincao={true}/>
       <Animal icone='🦒' nome='Girafa' peso={1200} extincao={true}/>
+      </Exibicao>
+
       <Exibicao abertura={new Date("2024-12-06T08:00:00.000-03:00")} fechamento={new Date("2024-12-06T16:00:00.000-03:00")}
-      cercado='
-      BI' />
-      <Animal icone='🦓' nome='Zebra' peso={200}/>
+      cercado='B1'
+      >
+      <Animal icone='🦓' nome='Zebra' peso={200} extincao={true}/>
+      <Animal icone='🦜' nome='Papagaio' peso={0.12}/>
+
+      </Exibicao>
     </div>
     
   )
