2832348bcd0e96d1a21caf715391db1737816f22
Pedro Bonfá <pedro.bonfa@estudante.ufjf.br>
2024-12-09 20:19:16 -0300

    adiciona filtro no mapa

--- src/App.tsx
+++ src/App.tsx
@@ -4,48 +4,56 @@ import Exibicao from "./Exibicao";
 import { ReactNode } from 'react';
 type AnimaisTuplaType = [string, string, number, boolean];
 
-const ANIMAIS:AnimaisTuplaType[] = [
-  
-  ["🦁", "Leão" ,190.37, true],
+const ANIMAIS: AnimaisTuplaType[] = [
+
+  ["🦁", "Leão", 190.37, true],
   ["🦒", "Girafa", 190.37, true],
   ["🦜", "Papagaio", 0.12, false],
-  ["🦓", "Zebra" ,200, true],
+  ["🦓", "Zebra", 200, true],
 
 ];
 
 function App() {
-    const exA1:ReactNode[] = [];
-    const exB2:ReactNode[] = ANIMAIS.map(function(animal){
-      return <Animal 
-        key={animal[1]}
-        icone={animal[0]}
-        nome={animal[1]}
-        peso={animal[2]}
-        extincao={animal[3]}
-      
-      />
-    });
+  const exA1: AnimaisTuplaType[] = ANIMAIS.filter(animal => animal[2] < 200.00);
+  const exB2: AnimaisTuplaType[] = ANIMAIS.filter(animal => animal[2] >= 200.00);
 
-  
 
-  return ( 
+  return (
     <div className='app'>
 
-      <Exibicao 
-      abertura={new Date("2024-12-06T08:00:00.000-03:00")} 
-      fechamento={new Date("2024-12-06T16:00:00.000-03:00")}
-      cercado='A1'
-      >{exA1}
-      
+      <Exibicao
+        abertura={new Date("2024-12-06T08:00:00.000-03:00")}
+        fechamento={new Date("2024-12-06T16:00:00.000-03:00")}
+        cercado='A1'
+      >{exA1.map((animal) => {
+        return <Animal
+          key={animal[1]}
+          icone={animal[0]}
+          nome={animal[1]}
+          peso={animal[2]}
+          extincao={animal[3]}
+
+        />
+      })}
+
       </Exibicao>
 
       <Exibicao abertura={new Date("2024-12-06T08:00:00.000-03:00")} fechamento={new Date("2024-12-06T16:00:00.000-03:00")}
-      cercado='B2'
-      >{exB2}
-      
+        cercado='B2'
+      >{exB2.map((animal) => {
+        return <Animal
+          key={animal[1]}
+          icone={animal[0]}
+          nome={animal[1]}
+          peso={animal[2]}
+          extincao={animal[3]}
+
+        />
+      })}
+
       </Exibicao>
     </div>
-    
+
   )
 }
 
